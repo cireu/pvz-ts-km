671028235fe0bd024889cee57df677bc42676ddf
cireu <all_but_last@163.com>
2018-09-17 19:52:25 +0800

    restruct pvz-km.ts

--- src/lib/pvz-km/pvz-km.ts
+++ src/lib/pvz-km/pvz-km.ts
@@ -8,35 +8,41 @@ const tasFns = tasFn.load()
 export class PVZKeyMacro {
   private static singleton: PVZKeyMacro
   private scene: SCENE
-  private hwnd: M.HWND; private pid?: number; private handle?: M.HANDLE
-  private constructor (gameName = 'Plants vs. Zombies', scene = SCENE.PE, hideBorder = false) {
-    const PROCESS_ALL_ACCESS = (0x00100000 | 0x000F0000 | 0xFFF)
-
+  private hwnd = {} as M.HWND; private pid = 0; private handle = {} as M.HANDLE
+  private constructor (gameName = 'Plants vs. Zombies', scene = SCENE.PE, resizeWindow = false) {
+    
     this.scene = scene
     
     //Find Game
+    this.findGame(gameName)
+
+    //HideBorder
+    if (resizeWindow) this.resizeWindow()
+  }
+
+  private findGame(gameName: string) {
+    const PROCESS_ALL_ACCESS = 0x00100000 | 0x000F0000 | 0xFFF
+    
     let classNamePtr = TEXT('MainWindow'), gameNamePtr = TEXT(gameName)
     this.hwnd = tasFns.FindWindowW(classNamePtr, gameNamePtr)
     let pidPtr = ref.alloc(W.LPDWORD) as FModel.FFIBuffer
-    if (this.hwnd) tasFns.GetWindowThreadProcessId(this.hwnd, pidPtr)
-    // this.pid = pidPtr.deref()
-    // if (this.pid !== 0) this.handle = tasFns.OpenProcess(PROCESS_ALL_ACCESS, 0, this.pid)
-    // console.log(`Successfully found game PID:${this.pid}`)
-    
-    //HideBorder
-    if (hideBorder) {
-      let windowStyle = tasFns.GetWindowLongPtrW(this.hwnd, -16)
-      windowStyle &= (
-        ~tasFn.constants.WS_CAPTION &
-        ~tasFn.constants.WS_THICKFRAME )
-      tasFns.SetWindowLongPtrW(this.hwnd, -16, windowStyle)
-      //
-      tasFns.SetWindowPos(this.hwnd, ref.NULL, 0, 0, 800, 600, (0x0002 | 0x0200 | 0x0020))
-    }
+    if (this.hwnd)
+      tasFns.GetWindowThreadProcessId(this.hwnd, pidPtr)
   }
 
-  static initialize (gameName?: string, scene?: SCENE, hideBorder?: boolean) {
-    PVZKeyMacro.singleton = PVZKeyMacro.singleton || new PVZKeyMacro(gameName, scene, hideBorder)
+  private resizeWindow() {
+    const WM_STYLE = -16
+    const ACTION_MODIFIER = 0x0002 | 0x0200
+    
+    let windowStyle = tasFns.GetWindowLongPtrW(this.hwnd, WM_STYLE)
+    windowStyle &= (~tasFn.constants.WS_CAPTION & ~tasFn.constants.WS_THICKFRAME)
+    
+    tasFns.SetWindowLongPtrW(this.hwnd, WM_STYLE, windowStyle)
+    tasFns.SetWindowPos(this.hwnd, ref.NULL, 0, 0, 800, 600, ACTION_MODIFIER)
+  }
+
+  static initialize (gameName?: string, scene?: SCENE, resizeWindow?: boolean) {
+    PVZKeyMacro.singleton = PVZKeyMacro.singleton || new PVZKeyMacro(gameName, scene, resizeWindow)
     return PVZKeyMacro.singleton
   }
 
